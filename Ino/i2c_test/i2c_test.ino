54c756d37bce3c6cd2ed6d54a374a1c6dcee858d
Jalil Morris <jmorris@g.harvard.edu>
2023-03-24 11:12:22 -0400

    Ino and ch101 driver

---
+++ Ino/i2c_test/i2c_test.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+
+#define RESET_N 12
+#define PROG 11
+
+void setup () {
+  // Set up GPIO
+  pinMode (RESET_N, OUTPUT);
+  digitalWrite (RESET_N, LOW);
+  pinMode (PROG, OUTPUT);
+  digitalWrite (PROG, LOW);
+
+  Wire.begin ();
+  Wire.setClock(1000 /* 40 kHz*/);
+  // Wire.setWireTimeout (5000 /* us */, false /* resetOnTimeout */);
+  // MCUCR |= (1<<PUD);
+  Serial.begin (9600);
+}
+
+void loop () {
+  // Enable
+  digitalWrite (RESET_N, HIGH);
+  delay (50);
+  digitalWrite (PROG, HIGH);
+  delay (50);
+
+  // Test bus
+  Wire.beginTransmission (0x45);
+  Wire.write (0);
+  // Wire.write (2);
+  Wire.endTransmission ();
+
+  Wire.requestFrom(0x45, 2);
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.println(int(c));
+  }
+
+  digitalWrite (PROG, LOW);
+  digitalWrite (RESET_N, LOW);
+
+  Serial.println("Completed Cycle\n\n");
+  delay (1000 /* ms */);
+}
